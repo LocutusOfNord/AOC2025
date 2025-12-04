360a951c347eafd168cec230c6b5981f6b58405a
LocutusOfNord <don.hillson@gmail.com>
2025-12-04 03:01:25 +0100

    Day 1

--- d1c2.py
+++ d1c2.py
@@ -6,4 +6,4 @@ with open("docs/d1c1.txt") as f:
         for i in range(0,int(line[1:])):
             pos = (pos + direc) % 100
             zcount += (pos == 0)
-#print("The arrow hit the 0 marker " + str(zcount) + " times.")
+print("The arrow hit the 0 marker " + str(zcount) + " times.")
